8329b06d6fc7c12f8bd31303d96b466d480837ab
spowers0409 <therealsjpowers04@gmail.com>
2022-10-15 07:09:41 -0500

    Added comments for clarity

--- Linear Search/Linear_Search.py
+++ Linear Search/Linear_Search.py
@@ -1,28 +1,26 @@
+# define search function 
 def search(arr, N, x):
- 
+    #iterate through array until match is found equal to userInput
     for i in range(0, N):
         if (arr[i] == x):
             return i
-    return -1
-
-#string = int(input("Enter a valie: "))
- 
+    return -1 
  
 # Driver Code
 if __name__ == "__main__":
+    # the array to iterate and return results from
     arr = [2, 4, 7, 10, 11 ,32 ,45, 87]
     print(arr)
+    # userInput to determine where in array a value is located
     userInput = int(input("Enter a value: "))
     x = userInput
     N = len(arr)
     output = "  was not found."
     outputResult = str(userInput) + str(output) 
  
-    # Function call
+    # Function call to print results of where in an array the value is located
     result = search(arr, N, x)
     if(result == -1):
-        #print(x + "Element is not present in array")
         print(outputResult)
     else:
-        # print("Element is present at index", result)
-        print("Found", x, "at index", result)
+        print("Found", x, "at index", result, "using Python")
